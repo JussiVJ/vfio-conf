4e380709839132a31686f744b22784d8239eb34e
JussiJ <jusku39@gmail.com>
2018-12-22 01:34:08 +0200

    Continued with the gui and added errordialogs.

--- gui.py
+++ gui.py
@@ -14,20 +14,23 @@ class MainWindow(Gtk.Window):
         BoxBasic.set_margin_right(6)
         self.add(BoxBasic)
 
-
         LabelMain = Gtk.Label("Basic configuration options")
         BoxBasic.add(LabelMain)
 
-        CheckVfio = Gtk.CheckButton("Vfio is compiled in")
-        CheckVfio.connect("toggled", self.vfio_integrated_checked)
-        BoxBasic.add(CheckVfio)
+        self.CheckVfio = Gtk.CheckButton("Vfio is compiled in")
+        self.CheckVfio.connect("toggled", self.vfio_integrated_checked)
+        BoxBasic.add(self.CheckVfio)
 
         PanedBasic = Gtk.HPaned()
         BoxBasic.add(PanedBasic)
 
+        PanedBasic2 = Gtk.HPaned()
+        BoxBasic.add(PanedBasic2)
+
         ButtonVfioEnable = Gtk.Button.new_with_label("Enable Vfio")
         ButtonVfioEnable.connect("clicked", self.enable_vfio)
         PanedBasic.add(ButtonVfioEnable)
+        ButtonVfioEnable.set_size_request(120, 0)
 
         ButtonVfioDisable = Gtk.Button.new_with_label("Disable Vfio")
         ButtonVfioDisable.connect("clicked", self.disable_vfio)
@@ -35,28 +38,43 @@ class MainWindow(Gtk.Window):
 
         ButtonEnableIommu = Gtk.Button.new_with_label("Enable Iommu")
         ButtonEnableIommu.connect("clicked", self.enable_iommu)
-        PanedBasic.add(ButtonEnableIommu)
+        PanedBasic2.add(ButtonEnableIommu)
 
+        ButtonDisableIommu = Gtk.Button.new_with_label("Disable Iommu")
+        ButtonDisableIommu.connect("clicked", self.disable_iommu)
+        PanedBasic2.add(ButtonDisableIommu)
+
+        FramePci = Gtk.Frame()
+
+#Buttons and other actions
     def vfio_integrated_checked(self, CheckVfio):
-        vfio_integrated = CheckKey.get_active()
+        vfio_integrated = CheckVfio.get_active()
+        print("vfio_integrated = "+str(CheckVfio.get_active()))
 
     def disable_vfio(self, ButtonVfioDisable):
-        mkinitcpio_config = '/etc/mkinitcpio.conf'
-        for line in fileinput.FileInput(mkinitcpio_config,inplace=1):
-            if 'vfio' in line:
-                line = line.replace(' vfio_pci vfio vfio_iommu_type1 vfio_virqfd', '')
-            print(line, end=" ")
+        if self.CheckVfio.get_active() == 'False':
+            for line in fileinput.FileInput("/etc/mkinitcpio.conf",inplace=1):
+                if 'vfio' in line:
+                    line = line.replace(" vfio_pci vfio vfio_iommu_type1 vfio_virqfd", "")
+                print(line, end=" ")
+        elif pci_chosen == 'true':
+            for line in fileinput.FileInput("etc/default/grub",inplace=1):
+                if "GRUB_CMDLINE_LINUX_DEFAULT=" in line:
+                    line = line.replace("GRUB_CMDLINE_LINUX_DEFAULT=","GRUB_CMDLINE_LINUX_DEFAULT="+" vfio_ids="+vfio_ids)
+                else:
+                    self.invalid_grub_conf()
 
     def enable_vfio(self, ButtonVfioEnable):
-        word = 'keyboard'
-        mkinitcpio_config = '/etc/mkinitcpio.conf'
-        for line in fileinput.FileInput(mkinitcpio_config,inplace=1):
-            if 'vfio' in line:
-                print(line, end=" ")
-            else:
-                if word in line:
-                    line = line.replace(word,word+' vfio_pci vfio vfio_iommu_type1 vfio_virqfd')
-                print(line, end=" ")
+        if self.CheckVfio.get_active() == 'False':
+            for line in fileinput.FileInput("/etc/mkinitcpio.conf",inplace=1):
+                if "vfio" in line:
+                    print(line, end=" ")
+                    self.vfio_already_enabled()
+                elif "keyboard" in line:
+                    line = line.replace("keyboard"+"keyboard"" vfio_pci vfio vfio_iommu_type1 vfio_virqfd")
+                    print(line, end=" ")
+                else:
+                    self.invalid_grub_conf()
 
     def disable_iommu(self, ButtonDisableIommu):
         word = 'intel_iommu=on amd_iommu=on iommu=on iommu=pt '
@@ -67,15 +85,33 @@ class MainWindow(Gtk.Window):
             print(line,end=" ")
 
     def enable_iommu(self, ButtonEnableIommu):
-        word = 'GRUB_CMDLINE_LINUX_DEFAULT="'
-        grub_config = '/etc/default/grub'
-        for line in fileinput.FileInput(grub_config, inplace=1):
-            if 'iommu=on' in line:
+        for line in fileinput.FileInput("/etc/default/grub", inplace=1):
+            if "iommu" in line:
                 print(line,end=" ")
             else:
-                if word in line:
-                    line = line.replace(word,word+'intel_iommu=on amd_iommu=on iommu=on iommu=pt ')
+                if "GRUB_CMDLINE_LINUX_DEFAULT=" in line:
+                    line = line.replace("GRUB_CMDLINE_LINUX_DEFAULT=","GRUB_CMDLINE_LINUX_DEFAULT="+"intel_iommu=on amd_iommu=on iommu=on iommu=pt ")
                     print(line,end=" ")
+
+#Errordialogs
+    def invalid_grub_conf(self, widget, data=None):
+        dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.INFO,
+            Gtk.ButtonsType.OK, "Invalid GRUB config!")
+        dialog.format_secondary_text(
+            "Please check your GRUB config file and if it's fine file create a new issue on github.")
+        dialog.run()
+
+        dialog.destroy()
+
+    def vfio_already_enabled(self, widget, data=None):
+        dialog = Gtk.MessageDialog(self, 0, Gtk.MessageType.INFO,
+            Gtk.ButtonsType.OK, "Vfio already enabled!")
+        dialog.format_secondary_text(
+            "Vfio should already be enable, but if it's not create a new issue on github.")
+        dialog.run()
+
+        dialog.destroy()
+
 
 
 main = MainWindow()
