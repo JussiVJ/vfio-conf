03dd153156198d23f644511eeeefdb99e3631332
Jussi <jusku39@gmail.com>
2019-01-28 13:27:24 +0200

    Removed outdated and unneeded modules that have been integrated into the main executable a long time ago

--- modules/iommu_disable.py
+++
@@ -1,8 +0,0 @@
-import fileinput
-
-word = 'intel_iommu=on amd_iommu=on iommu=on iommu=pt '
-grub_config = '/etc/default/grub'
-for line in fileinput.FileInput(grub_config, inplace=1):
-    if word in line:
-        line = line.replace(word,'')
-    print(line,end=" ")
--- modules/vfio_disable.py
+++
@@ -1,6 +0,0 @@
-import fileinput
-mkinitcpio_config = '/etc/mkinitcpio.conf'
-for line in fileinput.FileInput(mkinitcpio_config,inplace=1):
-    if 'vfio' in line:
-        line = line.replace(' vfio_pci vfio vfio_iommu_type1 vfio_virqfd', '')
-    print(line, end=" ")
