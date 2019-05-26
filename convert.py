2b1d3ee8934176710ea37dbdebdd6b0b3bf6a40f
JussiVJ <jusku39@gmail.com>
2019-05-26 13:46:19 +0300

    Updated the conversion scripts
    The conversion scripts should now be updated to work on the revamped version of the program

--- convert.py
+++ convert.py
@@ -1,6 +1,7 @@
 import fileinput
 import os
-for line in fileinput.FileInput("gui.py",inplace=1):
+
+for line in fileinput.FileInput("main.py",inplace=1):
     if 'testfilemodprobe' in line:
         line = line.replace("testfilemodprobe", "/etc/modprobe.d/vfioconf.conf")
     if 'testfilegrub' in line:
@@ -13,10 +14,17 @@ for line in fileinput.FileInput("gui.py",inplace=1):
         line = line.replace("testfilemodload", "/etc/modules/vfioconf.conf")
     print(line,end="")
 
-os.remove("testfilemodprobe")
-os.remove("testfilegrub")
-os.remove("testfilemkinitcpio")
-os.remove("testfileos")
-os.remove("testfilemodload")
-os.remove("test.py")
-os.remove("convert.py")
+for item in os.listdir("modules"):
+    if '.py' in item:
+        for line in fileinput.FileInput(item,inplace=1):
+            if 'testfilemodprobe' in line:
+                line = line.replace("testfilemodprobe", "/etc/modprobe.d/vfioconf.conf")
+            if 'testfilegrub' in line:
+                line = line.replace("testfilegrub", "/etc/default/grub")
+            if 'testfilemkinitcpio' in line:
+                line = line.replace("testfilemkinitcpio", "/etc/mkinitcpio.conf")
+            if 'testfileos' in line:
+                line = line.replace("testfileos", "/etc/os-release")
+            if 'testfilemodload' in line:
+                line = line.replace("testfilemodload", "/etc/modules/vfioconf.conf")
+            print(line,end="")
--- unconvert.py
+++ unconvert.py
@@ -1,5 +1,7 @@
 import fileinput
-for line in fileinput.FileInput("gui.py",inplace=1):
+import os
+
+for line in fileinput.FileInput("main.py",inplace=1):
     if 'testfilemodprobe' in line:
         line = line.replace("/etc/modprobe.d/vfioconf.conf", "testfilemodprobe")
     if 'testfilegrub' in line:
@@ -11,3 +13,18 @@ for line in fileinput.FileInput("gui.py",inplace=1):
     if 'testfilemodload' in line:
         line = line.replace("/etc/modules/vfioconf.conf", "testfilemodload")
     print(line,end="")
+
+for item in os.listdir("modules"):
+    if '.py' in item:
+        for line in fileinput.FileInput(item,inplace=1):
+            if 'testfilemodprobe' in line:
+                line = line.replace("/etc/modprobe.d/vfioconf.conf", "testfilemodprobe")
+            if 'testfilegrub' in line:
+                line = line.replace("/etc/default/grub", "testfilegrub")
+            if 'testfilemkinitcpio' in line:
+                line = line.replace("/etc/mkinitcpio.conf", "testfilemkinitcpio")
+            if 'testfileos' in line:
+                line = line.replace("/etc/os-release", "testfileos")
+            if 'testfilemodload' in line:
+                line = line.replace("/etc/modules/vfioconf.conf", "testfilemodload")
+            print(line,end="")
